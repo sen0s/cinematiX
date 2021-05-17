59bf3e14a20a9262a50643584ca1c05503ee2bda
kleanti bashalli <58070317+kleanthis2001@users.noreply.github.com>
2021-05-17 17:25:21 +0300

    Add files via upload

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,18 +1,60 @@
 import java.util.UUID;
 
 public class Reservation {
-
+    static int total = 0;
     private Movie movie;
-    //private Room room;
+    private Room room;
     private int seatNumber;
     private String ticketType;
     private UUID reservationID;
 
-    public Reservation(Movie movie, int seatNumber, String ticketType) {
+    public Reservation(Movie movie, int seatNumber, String ticketType,Room room) {
         this.movie = movie;
         this.seatNumber = seatNumber;
         this.ticketType = ticketType;
         this.reservationID = UUID.randomUUID();
+        this.room =  room;
+        total++;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public UUID getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(UUID reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
     }
 
     public void printInfo() {
---
+++ src/Room.java
@@ -0,0 +1,46 @@
+public class Room {
+    private String RoomID;
+    private int capacity;
+    private Database database;
+
+    public Room(String RoomID,int capacity) {
+    this.capacity =  capacity;
+    this.RoomID =  RoomID;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getRoomID() {
+        return RoomID;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setRoomID(String roomID) {
+        RoomID = roomID;
+    }
+
+    public boolean checkCapacity(){
+        if(getEmptySeats() == 0)
+            return false;//επιστροφη false  οταν ενα room ειναι γεματο
+        else
+            return true;//αν εχει κενες θεσεις επιστρεφει true
+    }
+
+    public  int getEmptySeats(){
+        int ReservedSeats=0;
+        for(Reservation reservations:database.allReservations){
+            if(reservations.getRoom().getRoomID() == this.getRoomID()){
+                ReservedSeats ++;
+            }
+
+        }
+
+        return (capacity-ReservedSeats);
+    }
+}
+
