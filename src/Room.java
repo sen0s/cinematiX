710749a196d7b2894a213c199415f2e2760a805e
kleanti bashalli <58070317+kleanthis2001@users.noreply.github.com>
2021-06-07 11:18:07 +0300

    room error fix

--- src/Room.java
+++ src/Room.java
@@ -1,21 +1,39 @@
 import java.io.Serializable;
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Room implements Serializable {
 
     private String RoomID;
     private int capacity;
     private ArrayList<Boolean> seats;
-    private HashMap<Date,Room> 
+    private HashMap<Date,Room> Reservation;
 
-    public Room(String RoomID,int capacity) {
+    public ArrayList<Boolean> getSeats() {
+		return seats;
+	}
+
+	public void setSeats(ArrayList<Boolean> seats) {
+		this.seats = seats;
+	}
+
+	public HashMap<Date, Room> getReservation() {
+		return Reservation;
+	}
+
+	public void setReservation(HashMap<Date, Room> reservation) {
+		Reservation = reservation;
+	}
+
+	public Room (String RoomID,int capacity) {
 
         this.capacity =  capacity;
         this.RoomID =  RoomID;
 
         this.seats = new ArrayList<>();
         for(int i=0;i<capacity;i++) {
-            this.seats.add(true); //True óçìáéíåé ïôé ç èåóç åéíáé áñ÷éêá áäåéá
+            this.seats.add(true); //True ÏƒÎ·Î¼Î±Î¹Î½ÎµÎ¹ Î¿Ï„Î¹ Î· Î¸ÎµÏƒÎ· ÎµÎ¹Î½Î±Î¹ Î±ÏÏ‡Î¹ÎºÎ± Î±Î´ÎµÎ¹Î±
         }
 
         for(Boolean b : seats) {
