01f16b00df23f966a18ee98a7806c68261ace5e7
Athanasios Senos <ath.sen@protonmail.com>
2021-06-08 15:29:47 +0300

    Configure BookTicketFrame to reserve seat when making reservation

--- src/Room.java
+++ src/Room.java
@@ -17,9 +17,6 @@ public class Room implements Serializable {
             this.seats.add(true); //True σημαινει οτι η θεση ειναι αρχικα αδεια
         }
 
-        for(Boolean b : seats) {
-            System.out.println(b);
-        }
     }
 
     public int getCapacity() {
@@ -39,7 +36,7 @@ public class Room implements Serializable {
     }
 
     public void reserveSeat(int seatNumber) {
-        seats.add(seatNumber, false);
+        seats.set(seatNumber-1, false);
     }
 
     public ArrayList<Integer> getEmptySeats() {
@@ -48,50 +45,11 @@ public class Room implements Serializable {
         for (int i=0;i<seats.size();i++){
             if (seats.get(i) == true){
                 emptySeats.add(i+1);
-//                System.out.println(emptySeats.get(i));
             }
         }
-
-
-//        for(Boolean b : seats) {
-//            if (b == true){
-//                emptySeats.add(seats.indexOf(b));
-//                System.out.println(seats.indexOf(b));
-//            }
-//        }
 
         return emptySeats;
     }
 
-//    public boolean checkCapacity(){
-//        if(getEmptySeats() == 0)
-//            return false;
-//        else
-//            return true;
-//    }
-
-//    public  int getEmptySeats(){
-//        int ReservedSeats=0;
-//        for(Reservation reservations:Database.allReservations){
-//            if(reservations.getRoom().getRoomID() == this.getRoomID()){
-//                ReservedSeats ++;
-//            }
-//
-//        }
-//
-//        freeSeats =  capacity - ReservedSeats;
-//
-//        return freeSeats;
-//    }
-//
-//    public void NumberOfFreeSeats() {
-//    	int j=0;
-//    	for(int i=0;i<capacity;i++) {
-//    		if(Seats[i] == true) {
-//    			FreeSeats[j] = i;
-//    			j++;
-//    		}
-//    	}
-//    }
 }
 
